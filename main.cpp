141804ee16ab149234a5348142d107fee4f9c421
TheLebaneseLegend <camconnelly20@outlook.com>
2025-12-08 20:55:48 -0500

    did a few changes, added a vector, need to figure out how to use it

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 int SCREEN_WIDTH = 640;
 int SCREEN_HEIGHT = 480;
-int RECT_WIDTH = 10;
-int RECT_HEIGHT = 10;
+int RECT_WIDTH = 4;
+int RECT_HEIGHT = 4;
 
 int main() {
 
@@ -16,20 +16,26 @@ int main() {
     std::vector<SDL_Rect> rects;
 
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+    window = SDL_CreateWindow("Random Walk c++", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, NULL, NULL);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
+    SDL_RenderPresent(renderer);
+
+    struct DrawnRect {
+        SDL_Rect rect;
+        SDL_Color color;
+    };
+
+    std::vector<DrawnRect> drawn_rects;
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_Rect rect1 = {(SCREEN_WIDTH / 2) - (RECT_WIDTH / 2), (SCREEN_HEIGHT / 2) - (RECT_HEIGHT / 2), RECT_WIDTH, RECT_HEIGHT};
-    SDL_RenderFillRect(renderer, &rect1);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_Rect rect2 = {(SCREEN_WIDTH / 2) - (RECT_WIDTH / 2), (SCREEN_HEIGHT / 2) - (RECT_HEIGHT / 2), RECT_WIDTH, RECT_HEIGHT};
-    SDL_RenderFillRect(renderer, &rect2);
-
-    SDL_RenderPresent(renderer);
+    drawn_rects.push_back(rect1);
+    //SDL_RenderFillRect(renderer, &rect1);
 
     bool running = true;
     SDL_Event event;
@@ -42,55 +48,35 @@ int main() {
         }
 
         int randNum1 = (rand() % 4) + 1;
-        int randNum2 = (rand() % 4) + 1;
 
         switch (randNum1) {
             case 1:
-                rect1.x += 10;
+                rect1.x += RECT_WIDTH;
                 break;
             case 2:
-                rect1.y += 10;
+                rect1.y += RECT_HEIGHT;
                 break;
             case 3:
-                rect1.x -= 10;
+                rect1.x -= RECT_WIDTH;
                 break;
             case 4:
-                rect1.y -= 10;
+                rect1.y -= RECT_HEIGHT;
                 break;
             default:
                 break;
         }
 
-        switch (randNum2) {
-            case 1:
-                rect2.x += 10;
-                break;
-            case 2:
-                rect2.y += 10;
-                break;
-            case 3:
-                rect2.x -= 10;
-                break;
-            case 4:
-                rect2.y -= 10;
-                break;
-            default:
-                break;
-        }
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect1);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &rect2);
 
         SDL_RenderPresent(renderer);
 
 
-        SDL_Delay(200);
+        SDL_Delay(150);
 
-        std::cout << randNum1 << " " << randNum2 << std::endl;
+        //std::cout << randNum1 << " " << randNum2 << std::endl;
     }
 
 
